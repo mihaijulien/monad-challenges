1adca3f31af37ce61561a2a97e1399eb8b70ec7f
mihaijulien <mihaijulien@gmail.com>
2025-03-15 16:03:53 +0200

    finish Set2

--- Set2.hs
+++ Set2.hs
@@ -5,9 +5,22 @@ module Set2 where
 
 import MCPrelude
 
-data Maybe a = Nothing | Just a deriving (Eq)
+-- Exercise 2.1 The Maybe Type
 
--- Exercise 2.1 Build a library of things that can fail
+data Maybe a = Nothing | Just a
+
+instance Show a => Show (Maybe a) where
+  -- show :: Maybe a -> String
+  show Nothing = "Nothing"
+  show (Just x) = "Just " ++ show x
+
+instance Eq a => Eq (Maybe a) where
+  -- (==) :: Maybe a -> Maybe a -> Bool
+  (==) Nothing Nothing = True
+  (==) (Just x) (Just y) = x == y
+  (==) _ _ = False
+
+-- Exercise 2.2 Build a library of things that can fail
 
 headMay :: [a] -> Maybe a
 headMay [] = Nothing
@@ -33,7 +46,9 @@ minimumMay :: Ord a => [a] -> Maybe a
 minimumMay [] = Nothing
 minimumMay (x:xs) = Just (foldl min x xs)
 
--- Exercise 2.2 Chains of Failing Computations
+-- Exercise 2.3 Chains of Failing Computations
+
+-- type GreekData = [(String, [Integer])]
 
 queryGreek :: GreekData -> String -> Maybe Double
 queryGreek greek key = case lookupMay key greek of
@@ -46,22 +61,48 @@ queryGreek greek key = case lookupMay key greek of
           Nothing -> Nothing
           Just hxs -> divMay (fromIntegral mxs) (fromIntegral hxs)
 
+-- Exercise 2.4 Generalizing chains of failures
+
 chain :: (a -> Maybe b) -> Maybe a -> Maybe b
 chain _ Nothing = Nothing
 chain f (Just a) = f a
 
 link :: Maybe a -> (a -> Maybe b) -> Maybe b
-link Nothing f = Nothing
-link (Just a) f = f a
+--link Nothing f = Nothing
+--link (Just a) f = f a
+link = flip chain
 
--- TODO
--- queryGreek2 :: GreekData -> String -> Maybe Double
+queryGreek2 :: GreekData -> String -> Maybe Double
+queryGreek2 greek key = lookupMay key greek `link` \xs ->
+    tailMay xs `link` \tl ->
+      maximumMay tl `link` \mx ->
+        headMay xs `link` \hd -> divMay (fromIntegral mx) (fromIntegral hd)
+
+-- Exercise 2.5 Chaining variations
+
+{-
+salaries :: [(String, Integer)]
+salaries = [ ("alice", 105000)
+           , ("bob", 90000)
+           , ("carol", 85000)
+           ]
+-}
 
 addSalaries :: [(String, Integer)] -> String -> String -> Maybe Integer
 addSalaries salaries p1 p2 =
     case (lookupMay p1 salaries, lookupMay p2 salaries) of
         (Just s1, Just s2) -> Just (s1 + s2)
         _ -> Nothing
+
+yLink :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+yLink f Nothing _ = Nothing
+yLink f _ Nothing = Nothing
+yLink f (Just a) (Just b) = Just (f a b)
+
+addSalaries2 :: [(String, Integer)] -> String -> String -> Maybe Integer
+addSalaries2 salaries p1 p2 = ylink (+) (lookupMay p1 salaries) (lookupMay p2 salaries)
+
+-- Exercise 2.6 Tailprod
 
 tailProd :: Num a => [a] -> Maybe a
 tailProd [] = Nothing
